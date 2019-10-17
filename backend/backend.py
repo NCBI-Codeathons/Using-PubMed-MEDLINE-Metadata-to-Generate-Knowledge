a7043132ebe23903e3a8057debba31e2a16f4eff
Eric Moyer <compu.moyer@gmail.com>
2019-10-17 10:10:59 -0400

    Fix formatting of backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -1,10 +1,8 @@
 import attr
-import pandas as pd
-import requests
 from Bio import Entrez
 from Bio import Medline
 from collections import Counter
-from typing import Dict, List, Optional
+from typing import Dict, List
 
 
 # Return list of pubmed ids for the query
@@ -19,24 +17,38 @@ def getPubMedIds(search_string: str, max_records: int):
     return result
 
 
-def searchQueryStringForMeshTermIntersection(first_mesh_terms: List[str], second_mesh_terms: List[str]) -> str:
-    """ Return the query string that would be used to search documents containing any of the first mesh terms and any of the second mesh terms"""
+def searchQueryStringForMeshTermIntersection(
+        first_mesh_terms: List[str],
+        second_mesh_terms: List[str]) -> str:
+    """Return the query string that would be used to search documents
+    containing any of the first mesh terms and any of the second mesh
+    terms"""
+
     def append_mh(all_terms: List[str]) -> List[str]:
         return [term + " [MH] " for term in all_terms]
+
     def ored_terms(all_terms: List[str]) -> str:
         return " OR ".join(append_mh(all_terms))
-    return "(" + ored_terms(first_mesh_terms) + ") AND (" + ored_terms(second_mesh_terms) + ")"
+
+    ored1 = ored_terms(first_mesh_terms)
+    ored2 = ored_terms(second_mesh_terms)
+    return "(" + ored1 + ") AND (" + ored2 + ")"
 
 
-def getPubMedIdsForMesh(first_mesh_terms: List[str], second_mesh_terms: List[str], max_records: int):
-    """ Return the pubmed ids that contain any of the first mesh terms and any of the second mesh terms """
-    return getPubMedIds(searchQueryStringForMeshTermIntersection(first_mesh_terms, second_mesh_terms), max_records)
+def getPubMedIdsForMesh(first_mesh_terms: List[str],
+                        second_mesh_terms: List[str],
+                        max_records: int):
+    """Return the pubmed ids that contain any of the first mesh terms and
+    any of the second mesh terms"""
+    return getPubMedIds(
+      searchQueryStringForMeshTermIntersection(
+        first_mesh_terms, second_mesh_terms), max_records)
 
 
 @attr.s
 class MeshAndId:
     """PubMed Id and List of associated MeSH terms"""
-    pmid : int = attr.ib()
+    pmid: int = attr.ib()
     mesh_terms: List[str] = attr.ib(factory=list)
 
 
@@ -45,21 +57,10 @@ def meshAndIdFromMedlineRecord(record: dict) -> MeshAndId:
     return MeshAndId(pmid=record["PMID"], mesh_terms=record.get("MH", []))
 
 
-def dataframeForMedlineRecord(record: dict):
-    """Return a data frame containing selected fields from record - If PMID is missing, it is None. Missing abstract is "NA". Other fields become empty string"""
-    df = pd.DataFrame()
-    df = df.append({
-        'PMID': record.get("PMID", None),
-        'Journal': record.get("JT", ""),
-        'PT': record.get("PT", ""),
-        'Title': record.get("TI", ""),
-        'Abstract': record.get("AB", "NA"),
-        'MeSH Terms' : record.get("MH", [])}, ignore_index=True)
-    return df
-
-
 def getMedLineRecordChunk(pmid_list: List[str], start: int):
-    """Return a generator of the 10000 (or fewer) medline records for a given list of pubmed ids starting at index start within the entire list"""
+    """Return a generator of the 10000 (or fewer) medline records for a
+    given list of pubmed ids starting at index start within the entire
+    list"""
     return Medline.parse(
         Entrez.efetch(
             db="pubmed",
@@ -74,7 +75,10 @@ def addMeshTermsToIds(pubmed_ids: List[str]) -> List[MeshAndId]:
     cur_start = 0
     result = []
     while(True):
-        mesh_chunk = [meshAndIdFromMedlineRecord(record) for record in getMedLineRecordChunk(pubmed_ids, cur_start) if "PMID" in record]
+        mesh_chunk = [
+          meshAndIdFromMedlineRecord(record) for record in
+          getMedLineRecordChunk(pubmed_ids, cur_start)
+          if "PMID" in record]
         result.extend(mesh_chunk)
         if len(mesh_chunk) < 10000:
             break
@@ -95,13 +99,18 @@ def removeQualifiers(mesh_term: str) -> str:
     return mesh_term.split("/")[0].replace("*", "")
 
 
-def countGroupedIds(term_to_group : Dict[str, str], id_meshes: List[MeshAndId]) -> Dict[str, int]:
-    """Return number of ids that had a mesh term that, after removing qualifiers, mapped to each group using the term_to_group dictionary
-    Mesh terms with no group are not counted
+def countGroupedIds(term_to_group: Dict[str, str],
+                    id_meshes: List[MeshAndId]) -> Dict[str, int]:
+    """Return number of ids that had a mesh term that, after removing
+    qualifiers, mapped to each group using the term_to_group
+    dictionary Mesh terms with no group are not counted
+
     """
     counts = Counter()
     for m_id in id_meshes:
         clean_terms = {removeQualifiers(term) for term in m_id.mesh_terms}
-        groups = Counter({term_to_group[clean_term] for clean_term in clean_terms if clean_term in term_to_group})
+        groups = Counter(
+          {term_to_group[clean_term] for clean_term in clean_terms
+           if clean_term in term_to_group})
         counts.update(groups)
     return dict(counts)
