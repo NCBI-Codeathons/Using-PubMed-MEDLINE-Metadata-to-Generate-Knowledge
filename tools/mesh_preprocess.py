1abf8f5c5a199d52dda798673256f78f7c058740
Victor Joukov <victzh@gmail.com>
2019-10-18 10:55:48 -0400

    Synonyms added, preprocessing tools initial checkin

---
+++ tools/mesh_preprocess.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+from typing import List, Dict
+import re
+import sys
+import os.path
+
+re_mh = re.compile(r"MH *= *(.+)")
+re_mn = re.compile(r"MN *= *(.+)")
+re_entry = re.compile(r"ENTRY *= *(.+)")
+
+re_line = re.compile(r"([A-Z]+) *= *(.+)")
+
+# MN (tree path), MH (main name)
+nodesList = []
+
+# main name, [ synonyms ]
+synonymList = []
+
+
+DECL_PREFIX = "d2020"
+SCR_PREFIX = "c2020"
+
+OUT_PREFIX = "mesh2020"
+
+
+def createNode(heading, nodes, synonyms):
+#    print(heading + " : " + " ".join(nodes))
+    for node in nodes:
+        nodesList.append((node, heading))
+    synonymList.append((heading, synonyms))
+    
+
+def parseMeSHDescriptors():
+    heading = ""
+    nodes = []
+    synonyms = []
+    # process Declarations file - record type D
+    # MH - MeSH heading, main name
+    # MN - node path
+    # ENTRY - synonym
+    with open(DECL_PREFIX+".bin", "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line: continue
+            if line == "*NEWRECORD":
+                if heading:
+                    createNode(heading, nodes, synonyms)
+                    heading = ""
+                    nodes = []
+                    synonyms = []
+                continue
+            mo = re_line.match(line)
+            if not mo: continue
+            ty, content = mo.groups()
+            if ty == "MH":
+                heading = content
+            elif ty == "MN":
+                nodes.append(content)
+            elif ty == "ENTRY":
+                synonym = content
+                try:
+                    i = content.index('|')
+                    synonym = content[:i]
+                except ValueError: pass
+                synonyms.append(synonym)
+    if heading:
+        createNode(heading, nodes, synonyms)
+    # Process SCR file - record type C
+    # NM - main name
+    # SY - synonym
+    # HM - reference to MeSH heading
+
+def writeFiles():
+    with open(OUT_PREFIX+".nodes", "w") as f:
+        for tree_node, heading in nodesList:
+            f.write(tree_node+'\t'+heading+'\n')
+    with open(OUT_PREFIX+".synonyms", "w") as f:
+        for name, synonyms in synonymList:
+            f.write(name+'\t'+'\t'.join(synonyms)+'\n')
+    
+
+
+def main(argv):
+    parseMeSHDescriptors()
+    writeFiles()
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
