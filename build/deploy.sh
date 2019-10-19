235cab30c547e77647be40735a356d30babe5d1c
Eugene Ivanchenko <me@eiva.info>
2019-10-19 12:27:45 -0400

    Deployed it in Azure on VPS and adapted to much smaller box size - Also fixed favicon - added some metatags

--- build/deploy.sh
+++ build/deploy.sh
@@ -9,7 +9,7 @@ cd ../
 SERVER=$USER@$IP
 
 
-ssh -i $KEY $SERVER 'docker stop pubmed'
+
 ssh -i $KEY $SERVER "rm -rf /home/$USER/dist.tar.gz /home/$USER/dist || True"
 scp -i $KEY ./dist.tar.gz  $SERVER:/home/$USER/dist.tar.gz
 
@@ -18,5 +18,6 @@ ssh -i $KEY $SERVER 'tar -zxvf dist.tar.gz'
 
 ssh -i $KEY $SERVER "cd /home/$USER/dist && docker build ./ --tag 'pubmed:0.1'"
 
-
-ssh -i $KEY $SERVER 'docker run -d --rm --name pubmed -p 80:8080 pubmed:0.1'
+ssh -i $KEY $SERVER 'docker stop pubmed'
+ssh -i $KEY $SERVER 'docker container rm pubmed'
+ssh -i $KEY $SERVER "docker run -d --restart always --name pubmed -p 80:8080 pubmed:0.1"
